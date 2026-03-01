99fc05a34bcd6f60ae67f632a40b1d991c63fb92
SShankar-ssh <sofieshankar@gmail.com>
2026-03-01 19:43:46 +0000

    Added a header.

--- question_1_sdtm/logR.R
+++ question_1_sdtm/logR.R
@@ -1,4 +1,18 @@
-library(logrx)
+###-----------------------------Header---------------------------------------###
+# Program : logR.R
+# Author  : S Shankar
+# Location: ~/question_1_sdtm/logR.R
+# Purpose : Wrapper program to create log for 01_create_ds_domain.R with logrx.
+#------------------------------------------------------------------------------#
+# Modification History
+#  01MAR2026 S Shankar
+#  Initiated program
+###--------------------------------------------------------------------------###
+
+source("functions.R")
+
+pkgs <- c("logrx")
+install_and_load(pkgs)
 
 log_remove()
 
