4c45a31a5a0c30790d4059c0b5da030b840a24af
SShankar-ssh <sofieshankar@gmail.com>
2026-03-01 07:47:27 +0000

    Included functions.R as its being called by 01_create_ds_domain.R.

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -54,80 +54,65 @@ ds <-
     raw_unk = c("UN", "UNK")
   ) 
 
-  ds2 <- ds_raw %>%
-  mutate(
-    #Copy IT.DSTERM or OTHERSP to DSTERM
-    #Copy IT.DSDECOD or OTHERSP to DSDECOD
-    DSDECOD = if_else(
-      is.na(OTHERSP),
-      IT.DSDECOD,
-      OTHERSP
-    ),
-    DSTERM = if_else(
-      is.na(OTHERSP),
-      IT.DSTERM,
-      OTHERSP
-    ),
-    DSCAT = case_when(
-      !is.na(OTHERSP)             ~ "OTHER EVENT",
-      IT.DSDECOD == "Randomized"  ~ "PROTOCOL MILESTONE",
-      TRUE                       ~ "DISPOSITION EVENT"
-    ),
-    VISITNUM = case_when(
-      INSTANCE == "Screening"            ~ 1,
-      INSTANCE == "Visit 1"              ~ 2,
-      INSTANCE == "Screening 1"          ~ 1,
-      INSTANCE == "Screening 2"          ~ 2,
-      INSTANCE == "Baseline"             ~ 3,
-      INSTANCE == "Week 2"               ~ 4,
-      INSTANCE == "Week 4"               ~ 5,
-      INSTANCE == "Week 6"               ~ 7,
-      INSTANCE == "Week 8"               ~ 8,
-      INSTANCE == "Week 12"              ~ 9,
-      INSTANCE == "Week 16"              ~ 10,
-      INSTANCE == "Week 20"              ~ 11,
-      INSTANCE == "Week 24"              ~ 12,
-      INSTANCE == "Week 26"              ~ 13,
-      INSTANCE == "Retrieval"            ~ 201,
-      INSTANCE == "Unscheduled 3.1"      ~ 3.1,
-      INSTANCE == "Ambul ECG Placement"  ~ 3.5,
-      INSTANCE == "Ambul ECG Removal"    ~ 6,
-      TRUE                               ~ NA_real_
-    ),
-    VISIT = case_when(
-      INSTANCE == "Screening"            ~ "SCREENING",
-      INSTANCE == "Visit 1"              ~ "VISIT 1",
-      INSTANCE == "Screening 1"          ~ "SCREENING 1",
-      INSTANCE == "Screening 2"          ~ "SCREENING 2",
-      INSTANCE == "Baseline"             ~ "BASELINE",
-      INSTANCE == "Week 2"               ~ "WEEK 2",
-      INSTANCE == "Week 4"               ~ "WEEK 4",
-      INSTANCE == "Week 6"               ~ "WEEK 6",
-      INSTANCE == "Week 8"               ~ "WEEK 8",
-      INSTANCE == "Week 12"              ~ "WEEK 12",
-      INSTANCE == "Week 16"              ~ "WEEK 16",
-      INSTANCE == "Week 20"              ~ "WEEK 20",
-      INSTANCE == "Week 24"              ~ "WEEK 24",
-      INSTANCE == "Week 26"              ~ "WEEK 26",
-      INSTANCE == "Retrieval"            ~ "RETRIEVAL",
-      INSTANCE == "Unscheduled 3.1"      ~ "UNSCHEDULED 3.1",
-      INSTANCE == "Ambul ECG Placement"  ~ "AMBUL ECG PLACEMENT",
-      INSTANCE == "Ambul ECG Removal"    ~ "AMBUL ECG REMOVAL",
-      TRUE                               ~ NA_character_
-    ),
-      STUDYID = ds_raw$STUDY,
-      DOMAIN = "DS",
-      USUBJID = paste0("01-", ds_raw$PATNUM),
-      DSDECOD = toupper(DSDECOD),
-      DSTERM  = toupper(DSTERM)
-    ) |>
-    select(oak_id, patient_number, DSTERM, DSDECOD, DSCAT, STUDYID, DOMAIN,
-           USUBJID, VISIT, VISITNUM)
-
-  ds3 <- left_join(ds, ds2, by = c("oak_id", "patient_number"))
+## ----r------------------------------------------------------------------------
+ds2 <- ds_raw %>%mutate(  
+  #Copy IT.DSTERM or OTHERSP to DSTERM  
+  #Copy IT.DSDECOD or OTHERSP to DSDECOD  
+  DSDECOD = if_else(    is.na(OTHERSP),    IT.DSDECOD,    OTHERSP  ),  
+  DSTERM = if_else(    is.na(OTHERSP),    IT.DSTERM,    OTHERSP  ),  
+  DSCAT = case_when(    !is.na(OTHERSP)             ~ "OTHER EVENT",    
+                        IT.DSDECOD == "Randomized"  ~ "PROTOCOL MILESTONE",    
+                        TRUE                       ~ "DISPOSITION EVENT"  ),  
+  VISITNUM = case_when( INSTANCE == "Screening"            ~ 1,    
+                        INSTANCE == "Visit 1"              ~ 2,    
+                        INSTANCE == "Screening 1"          ~ 1,    
+                        INSTANCE == "Screening 2"          ~ 2,    
+                        INSTANCE == "Baseline"             ~ 3,    
+                        INSTANCE == "Week 2"               ~ 4,    
+                        INSTANCE == "Week 4"               ~ 5,   
+                        INSTANCE == "Week 6"               ~ 7,    
+                        INSTANCE == "Week 8"               ~ 8,    
+                        INSTANCE == "Week 12"              ~ 9,    
+                        INSTANCE == "Week 16"              ~ 10,    
+                        INSTANCE == "Week 20"              ~ 11,    
+                        INSTANCE == "Week 24"              ~ 12,    
+                        INSTANCE == "Week 26"              ~ 13,    
+                        INSTANCE == "Retrieval"            ~ 201,    
+                        INSTANCE == "Unscheduled 3.1"      ~ 3.1,    
+                        INSTANCE == "Ambul ECG Placement"  ~ 3.5,    
+                        INSTANCE == "Ambul ECG Removal"    ~ 6,    
+                        TRUE                               ~ NA_real_  ), 
+  VISIT = case_when(    INSTANCE == "Screening"            ~ "SCREENING",    
+                        INSTANCE == "Visit 1"              ~ "VISIT 1",    
+                        INSTANCE == "Screening 1"          ~ "SCREENING 1",    
+                        INSTANCE == "Screening 2"          ~ "SCREENING 2",    
+                        INSTANCE == "Baseline"             ~ "BASELINE",    
+                        INSTANCE == "Week 2"               ~ "WEEK 2",    
+                        INSTANCE == "Week 4"               ~ "WEEK 4",   
+                        INSTANCE == "Week 6"               ~ "WEEK 6",    
+                        INSTANCE == "Week 8"               ~ "WEEK 8",   
+                        INSTANCE == "Week 12"              ~ "WEEK 12",   
+                        INSTANCE == "Week 16"              ~ "WEEK 16",   
+                        INSTANCE == "Week 20"              ~ "WEEK 20",    
+                        INSTANCE == "Week 24"              ~ "WEEK 24",    
+                        INSTANCE == "Week 26"              ~ "WEEK 26",    
+                        INSTANCE == "Retrieval"            ~ "RETRIEVAL",   
+                        INSTANCE == "Unscheduled 3.1"      ~ "UNSCHEDULED 3.1",   
+                        INSTANCE == "Ambul ECG Placement"  ~ "AMBUL ECG PLACEMENT",   
+                        INSTANCE == "Ambul ECG Removal"    ~ "AMBUL ECG REMOVAL",   
+                        TRUE                               ~ NA_character_  ),    
+  STUDYID = ds_raw$STUDY,    
+  DOMAIN = "DS",    
+  USUBJID = paste0("01-", ds_raw$PATNUM),    
+  DSDECOD = toupper(DSDECOD),    
+  DSTERM  = toupper(DSTERM)  ) |>  
+  select(oak_id, patient_number, DSTERM, DSDECOD, DSCAT, STUDYID, DOMAIN,
+         USUBJID, VISIT, VISITNUM)
+## ----r------------------------------------------------------------------------
+ds3 <- left_join(ds, ds2, by = c("oak_id", "patient_number"))
  
-
-  ds4 <- ds3 |>
+## ----r------------------------------------------------------------------------
+ds4 <- ds3 |>
     derive_seq(
       tgt_var  = "DSSEQ",
       rec_vars = c("USUBJID", "DSTERM")
@@ -142,5 +127,7 @@ ds <-
       "STUDYID", "DOMAIN", "USUBJID", "DSSEQ", "DSTERM", "DSDECOD", 
       "DSCAT", "VISITNUM", "VISIT", "DSDTC", "DSSTDTC", "DSSTDY"
     )
+
+## ----r------------------------------------------------------------------------
 #Export SDTM dataset
 write_csv(ds4, "/cloud/project/question_1_sdtm/ds.csv")
